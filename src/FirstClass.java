434cab20394c9f361b716254a5c7956fd0967c0d
Jayadeepkasse <153552048+Jayadeepkasse@users.noreply.github.com>
2025-01-07 07:02:56 -0500

    1.Created new Class 'SecondClass.java'.

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -3,6 +3,6 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-        System.out.println("Hello, Jay!");
+        System.out.print("Hello, Jay!");
     }
 }
