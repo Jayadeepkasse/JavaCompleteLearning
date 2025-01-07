f862aa1fdd9a1bb53c594b592fd3c90889f7d612
Jayadeepkasse <153552048+Jayadeepkasse@users.noreply.github.com>
2025-01-07 07:07:47 -0500

    Optimized Code Lines one SecondClass.java. Removed Line6 from'println' to 'print'.

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -3,7 +3,7 @@ public class SecondClass {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        System.out.println("Hello Jay");
+        System.out.print("Hello Jay");
 
     }
 }
